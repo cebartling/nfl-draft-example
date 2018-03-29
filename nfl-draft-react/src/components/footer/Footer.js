302d7838c1a9222dbdbc90a7c0a5dc880d0c313a
Christopher Bartling <chris.bartling@gmail.com>
2018-03-29 11:43:02 -0500

    Getting Bootstrap 4 working.

--- nfl-draft-react/src/components/footer/Footer.js
+++ nfl-draft-react/src/components/footer/Footer.js
@@ -6,35 +6,13 @@ class Footer extends Component {
     static defaultProps = {};
 
     render() {
+        const year = new Date().getFullYear();
+
         return (
             <footer className="footer">
                 <div className="container-fluid">
-                    <nav>
-                        <ul>
-                            <li>
-                                <a href="https://www.creative-tim.com">
-                                    Creative Tim
-                                </a>
-                            </li>
-                            <li>
-                                <a href="http://presentation.creative-tim.com">
-                                    About Us
-                                </a>
-                            </li>
-                            <li>
-                                <a href="http://blog.creative-tim.com">
-                                    Blog
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
                     <div className="copyright">
-                        &copy;
-                        <script>
-                            document.write(new Date().getFullYear())
-                        </script>, Designed by
-                        <a href="https://www.invisionapp.com" target="_blank">Invision</a>. Coded by
-                        <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>.
+                        &copy; {year}, Christopher Bartling.
                     </div>
                 </div>
             </footer>
--- nfl-draft-react/src/containers/WelcomeViewContainer.js
+++ nfl-draft-react/src/containers/WelcomeViewContainer.js
@@ -1,40 +1,23 @@
 import React, {Component} from 'react';
 import './WelcomeViewContainer.css';
 import Nav from "../components/nav/Nav";
+import Footer from "../components/footer/Footer";
 
 
 class WelcomeViewContainer extends Component {
     static propTypes = {};
     static defaultProps = {};
 
-    // render() {
-    //     return (
-    //         <div className="wrapper ">
-    //             <Sidebar/>
-    //             <div className="main-panel">
-    //                 <PanelHeader title={'Welcome'}/>
-    //                 <div className="content">
-    //                     <div className="row">
-    //                         <div className="col-md-12">
-    //                             <Card title="Mock Drafts"/>
-    //                         </div>
-    //                     </div>
-    //                     <div className="row">
-    //                         <div className="col-md-12">
-    //                             <Card title="Player Bios"/>
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //                 <Footer/>
-    //             </div>
-    //         </div>
-    //     );
-    // }
-
     render() {
         return (
             <div>
                 <Nav/>
+                <main role="main" className="container">
+                    <div className="">
+                        <h1>Welcome</h1>
+                    </div>
+                </main>
+                <Footer/>
             </div>
         );
     }
