aac2e62f0924501aa6086f924c174965a5beaee0
Christopher Bartling <chris.bartling@gmail.com>
2018-03-29 11:23:05 -0500

    Bootstrap 4 now working.

--- nfl-draft-react/src/components/nav/Nav.js
+++ nfl-draft-react/src/components/nav/Nav.js
@@ -7,68 +7,39 @@ class Nav extends Component {
 
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-transparent navbar-absolute bg-primary fixed-top">
-                <div className="container-fluid">
-                    <div className="navbar-wrapper">
-                        <div className="navbar-toggle">
-                            <button type="button" className="navbar-toggler">
-                                <span className="navbar-toggler-bar bar1"/>
-                                <span className="navbar-toggler-bar bar2"/>
-                                <span className="navbar-toggler-bar bar3"/>
-                            </button>
-                        </div>
-                        <a className="navbar-brand" href="#pablo">Table List</a>
-                    </div>
-                    {/*<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation"*/}
-                    {/*aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">*/}
-                    {/*<span className="navbar-toggler-bar navbar-kebab"/>*/}
-                    {/*<span className="navbar-toggler-bar navbar-kebab"/>*/}
-                    {/*<span className="navbar-toggler-bar navbar-kebab"/>*/}
-                    {/*</button>*/}
-                    <div className="collapse navbar-collapse justify-content-end" id="navigation">
-                        {/*<form>*/}
-                        {/*<div className="input-group no-border">*/}
-                        {/*<input type="text" value="" className="form-control" placeholder="Search..."/>*/}
-                        {/*<span className="input-group-addon">*/}
-                        {/*<i className="now-ui-icons ui-1_zoom-bold"/>*/}
-                        {/*</span>*/}
-                        {/*</div>*/}
-                        {/*</form>*/}
-                        <ul className="navbar-nav">
-                            {/*<li className="nav-item">*/}
-                            {/*<a className="nav-link" href="#pablo">*/}
-                            {/*<i className="now-ui-icons media-2_sound-wave"/>*/}
-                            {/*<p>*/}
-                            {/*<span className="d-lg-none d-md-block">Stats</span>*/}
-                            {/*</p>*/}
-                            {/*</a>*/}
-                            {/*</li>*/}
-                            {/*<li className="nav-item dropdown">*/}
-                            {/*<a className="nav-link dropdown-toggle" href="http://example.com"*/}
-                            {/*id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"*/}
-                            {/*aria-expanded="false">*/}
-                            {/*<i className="now-ui-icons location_world"/>*/}
-                            {/*<p>*/}
-                            {/*<span className="d-lg-none d-md-block">Some Actions</span>*/}
-                            {/*</p>*/}
-                            {/*</a>*/}
-                            {/*<div className="dropdown-menu dropdown-menu-right"*/}
-                            {/*aria-labelledby="navbarDropdownMenuLink">*/}
-                            {/*<a className="dropdown-item" href="#">Action</a>*/}
-                            {/*<a className="dropdown-item" href="#">Another action</a>*/}
-                            {/*<a className="dropdown-item" href="#">Something else here</a>*/}
-                            {/*</div>*/}
-                            {/*</li>*/}
-                            {/*<li className="nav-item">*/}
-                            {/*<a className="nav-link" href="#pablo">*/}
-                            {/*<i className="now-ui-icons users_single-02"/>*/}
-                            {/*<p>*/}
-                            {/*<span className="d-lg-none d-md-block">Account</span>*/}
-                            {/*</p>*/}
-                            {/*</a>*/}
-                            {/*</li>*/}
-                        </ul>
-                    </div>
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        {/*<li className="nav-item">*/}
+                            {/*<a className="nav-link" href="#">Link</a>*/}
+                        {/*</li>*/}
+                        {/*<li className="nav-item">*/}
+                            {/*<a className="nav-link disabled" href="#">Disabled</a>*/}
+                        {/*</li>*/}
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="http://example.com" id="dropdown01"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                            <div className="dropdown-menu" aria-labelledby="dropdown01">
+                                <a className="dropdown-item" href="#">Action</a>
+                                <a className="dropdown-item" href="#">Another action</a>
+                                <a className="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>
+                    </ul>
+                    {/*<form className="form-inline my-2 my-lg-0">*/}
+                        {/*<input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">*/}
+                            {/*<button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>*/}
+                    {/*</form>*/}
                 </div>
             </nav>
         );
--- nfl-draft-react/src/containers/WelcomeViewContainer.js
+++ nfl-draft-react/src/containers/WelcomeViewContainer.js
@@ -1,35 +1,40 @@
 import React, {Component} from 'react';
 import './WelcomeViewContainer.css';
-import Sidebar from "../components/sidebar/Sidebar";
-import Footer from "../components/footer/Footer";
-import PanelHeader from "../components/header/PanelHeader";
-import Card from "../components/card/Card";
+import Nav from "../components/nav/Nav";
 
 
 class WelcomeViewContainer extends Component {
     static propTypes = {};
     static defaultProps = {};
 
+    // render() {
+    //     return (
+    //         <div className="wrapper ">
+    //             <Sidebar/>
+    //             <div className="main-panel">
+    //                 <PanelHeader title={'Welcome'}/>
+    //                 <div className="content">
+    //                     <div className="row">
+    //                         <div className="col-md-12">
+    //                             <Card title="Mock Drafts"/>
+    //                         </div>
+    //                     </div>
+    //                     <div className="row">
+    //                         <div className="col-md-12">
+    //                             <Card title="Player Bios"/>
+    //                         </div>
+    //                     </div>
+    //                 </div>
+    //                 <Footer/>
+    //             </div>
+    //         </div>
+    //     );
+    // }
+
     render() {
         return (
-            <div className="wrapper ">
-                <Sidebar/>
-                <div className="main-panel">
-                    <PanelHeader title={'Welcome'}/>
-                    <div className="content">
-                        <div className="row">
-                            <div className="col-md-12">
-                                <Card title="Mock Drafts"/>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-md-12">
-                                <Card title="Player Bios"/>
-                            </div>
-                        </div>
-                    </div>
-                    <Footer/>
-                </div>
+            <div>
+                <Nav/>
             </div>
         );
     }
