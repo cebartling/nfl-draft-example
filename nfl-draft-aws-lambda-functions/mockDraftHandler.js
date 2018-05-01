76965f413eb4dc22122d8d5cb60b064c35a3337f
Christopher Bartling <chris.bartling@gmail.com>
2018-04-30 21:54:17 -0500

    Create mock draft function working.

--- nfl-draft-aws-lambda-functions/mockDraftHandler.js
+++ nfl-draft-aws-lambda-functions/mockDraftHandler.js
@@ -1,16 +1,24 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const uuidv4 = require('uuid/v4');
+const moment = require('moment');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
+
 module.exports.createMockDraft = async (event) => {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    const mockDraftId = uuidv4();
     const params = {
-        TableName: 'icecreams',
+        TableName: 'nfl-draft-machine-MockDrafts',
         Item: {
-            icecreamid: request.body.icecreamId,
-            name: request.body.name // your icecream name
+            MockDraftId: mockDraftId,
+            Name: event.mockDraftName,
+            LastUpdated: moment().toISOString()
         }
     };
 
-    return dynamoDb.put(params).promise(); // returns dynamo result
+    const promise1 = await dynamoDb.put(params).promise(); // returns dynamo result
+
+    return promise1;
 };
